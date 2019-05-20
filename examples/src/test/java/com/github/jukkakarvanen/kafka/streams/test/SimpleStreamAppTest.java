7bfea5ebc9daa6b8e41177c8f1a54d19538cabc4
Jukka Karvanen <48978068+jukkakarvanen@users.noreply.github.com>
2019-05-20 16:19:53 +0300

    Typo

--- examples/src/test/java/com/github/jukkakarvanen/kafka/streams/test/SimpleStreamAppTest.java
+++ examples/src/test/java/com/github/jukkakarvanen/kafka/streams/test/SimpleStreamAppTest.java
@@ -39,7 +39,7 @@ import static org.junit.Assert.assertNull;
 /**
  * Unit test of {@link SimpleStreamApp} stream using TopologyTestDriver.
  */
-public class SimpleStramAppTest {
+public class SimpleStreamAppTest {
 
     private TopologyTestDriver testDriver;
     private TestInputTopic<String, String> inputTopic;
